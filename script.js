3074a1a07758ffc6e90f9def3faed16935b12b0d
TLCroatt <trishacroatt@gmail.com>
2020-07-26 10:09:43 -0600

    add event listeners and local storage

--- script.js
+++ script.js
@@ -1,14 +1,76 @@
+//add moment.js and display on calender
+$(document).ready(function () {
+    console.log("working");
+    let momentVar = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+    let time = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"]
 
-    $(document).ready(function () {
-        console.log("working");
-        let momentVar = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-        let hours = ["9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"]
-
-        //display current day at top of calender
-       function displayDate() {
-           let currentDay = $("#currentDay").text(momentVar);
-           return currentDay;
+    //display current day at top of calender
+    function displayDate() {
+        let currentDay = $("#currentDay").text(momentVar);
+        return currentDay;
         
-       }
-       displayDate()
-    })
+    }
+    displayDate()
+
+    var toDoItem = []
+    let timeID = document.getElementById("id");
+
+    //retreive saved items from local storage
+    if (localStorage.getItem("items") !== null) {
+       toDoItem = JSON.parse(localStorage.getItem("items"));
+       for (i = 0; i < toDoItem.length; i++) {
+            var hour = Object.keys(toDoItem[i])[0];
+            var text = toDoItem[i][hour];
+            console.log(hour);
+            console.log(text);
+            $(".description").each(function() {
+               if ($(this).attr("id") === hour) {
+                   $(this).val(text);
+               }
+            })
+        
+       } 
+    }
+
+
+
+    //event listener for save buttons to save to local storage
+    $(".saveBtn").click(
+        function() {
+            var text = $(this).siblings("textarea").val().trim();
+            var hours = $(this).siblings("textarea").attr("id");
+            var object = {};
+            object[hours] = text;
+            console.log(text, hours);
+            toDoItem.push(object);
+            console.log(toDoItem);
+            localStorage.setItem("items", JSON.stringify(toDoItem));
+
+         }
+    )
+
+
+    //loop through time divs - if id > than current time, change colors
+    let newDate = new Date(currentDay);
+    let now = newDate.getHours();
+    for (var i = 0; i < timeID.length; i++) {
+        if (now > timeID[i]) {
+            $("#textarea").addClass("past");
+            console.log("past");
+        } //else if (now >= time[i] && now < time[i]++) {
+            //$("#textarea").addClass("present");
+            //console.log("present");
+        //} else if (now < time[i]) {
+           // $("#textarea").addClass("future");
+           // console.log("future");
+       // }
+            
+
+    };
+
+
+
+
+});
+
+
