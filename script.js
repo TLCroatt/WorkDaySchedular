5f253494e8a5ed28049b92f81f1c6cfb22781d61
TLCroatt <trishacroatt@gmail.com>
2020-07-26 12:54:12 -0600

    removed console logs, edited ReadMe

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
 $(document).ready(function () {
     console.log("working");
     let momentVar = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-    let time = ["9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00"]
-
+    
     //display current day at top of calender
     function displayDate() {
         let currentDay = $("#currentDay").text(momentVar);
@@ -13,17 +12,13 @@ $(document).ready(function () {
     displayDate()
 
     let toDoItem = []
-    let timeID = document.getElementById("id");
-    console.log(timeID);
-
+    
     //retreive saved items from local storage
     if (localStorage.getItem("items") !== null) {
        toDoItem = JSON.parse(localStorage.getItem("items"));
        for (i = 0; i < toDoItem.length; i++) {
             var hour = Object.keys(toDoItem[i])[0];
             var text = toDoItem[i][hour];
-            console.log(hour);
-            console.log(text);
             $(".description").each(function() {
                if ($(this).attr("id") === hour) {
                    $(this).val(text);
@@ -34,7 +29,6 @@ $(document).ready(function () {
     }
 
 
-
     //event listener for save buttons to save to local storage
     $(".saveBtn").click(
         function() {
@@ -42,9 +36,7 @@ $(document).ready(function () {
             var hours = $(this).siblings("textarea").attr("id");
             var object = {};
             object[hours] = text;
-            //console.log(text, hours);
             toDoItem.push(object);
-            //console.log(toDoItem);
             localStorage.setItem("items", JSON.stringify(toDoItem));
 
          }
@@ -53,25 +45,18 @@ $(document).ready(function () {
 
     //loop through time divs - if id > than current time, change colors
     let currentHour = moment().hours();
-    console.log(currentHour)
-    //let now = new Date($.now());
-    //console.log(new Date($.now()));
-    //console.log(typeof now)
-    //console.log(now)
+    
     $(".description").each(function() {
         var blockHour = parseInt($(this).attr("id").split("-")[1]);
         if (currentHour > blockHour) {
             $(this).addClass("past");
-            console.log("past");
         } else if (currentHour === blockHour) {
             $(this).remove("past");
             $(this).addClass("present");
-            console.log("present");
         } else if (currentHour < blockHour) {
             $(this).remove("past");
             $(this).remove("present");
             $(this).addClass("future");
-           console.log("future");
         }
             
 
